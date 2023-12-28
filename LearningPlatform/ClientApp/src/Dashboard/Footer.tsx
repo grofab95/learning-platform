0bbdf09128a4b6089b43d5dfbf0d60f41ffa1ea8
Fabian Grochla <f.grochla@inex.com.pl>
2023-12-28 08:51:52 +0100

    Update footer year

--- LearningPlatform/ClientApp/src/Dashboard/Footer.tsx
+++ LearningPlatform/ClientApp/src/Dashboard/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer: React.FC = () => {
     return (
         <div className="app-footer px-3">
-            <span>&copy;2023 fgCode</span>
+            <span>&copy;2024 fgCode</span>
             <span>Version: 1.0.0</span>
         </div>
     );
